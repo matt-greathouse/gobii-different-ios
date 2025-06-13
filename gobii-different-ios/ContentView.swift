96643bcd687757223429f61422f62ac3b3df529e
matt-greathouse <matt5316@gmail.com>
2025-06-12 20:01:48 -0400

    Initial Commit

---
+++ gobii-different-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  gobii-different-ios
+//
+//  Created by Matt Greathouse on 6/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ gobii-different-ios/gobii_different_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  gobii_different_iosApp.swift
+//  gobii-different-ios
+//
+//  Created by Matt Greathouse on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct gobii_different_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
